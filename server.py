9ee1ba997b599e6de0dc4adfbdcda3361a9d8378
singram11 <ingramsarah11@gmail.com>
2021-08-14 16:00:21 -0700

    change to prod flask add server config files

--- server.py
+++ server.py
@@ -270,4 +270,5 @@ def remove_list_items(list_item_id):
 
 if __name__ == "__main__":
     connect_to_db(app)
-    app.run(host="127.0.0.1", debug=True)
+    # app.run(host="127.0.0.1", debug=True)
+    app.run()
